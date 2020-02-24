e6190e1f663f59b343ea62a72f22c6514ba0a4c6
Ziv Pizanti <zivp@wix.com>
2020-02-24 16:48:21 +0200

    wip

--- __tests__/counter.spec.js
+++ __tests__/counter.spec.js
@@ -9,19 +9,19 @@ describe('Counter', () => {
 
   describe('constructor', () => {
     it('should create a new count property', () => {
-      expect(counter._count).toBe(0);
+      expect(counter._count).toBe(1);
     });
   });
 
   describe('increment', () => {
     it('should increment the counter by 1 if nothing provided', () => {
       counter.increment();
-      expect(counter._count).toBe(1);
+      expect(counter._count).toBe(2);
     });
 
     it('should increment the counter by the provided number', () => {
       counter.increment(5);
-      expect(counter._count).toBe(5);
+      expect(counter._count).toBe(6);
     });
   });
 });
