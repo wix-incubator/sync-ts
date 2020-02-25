631b04eeee2dca1d4b29e3aef4806e8e4ffe00df
OmerB <omerb@wix.com>
2020-02-25 16:39:17 +0200

    Chore(sync-tc): trying to fix build

--- __tests__/get-files-content.spec.js
+++ __tests__/get-files-content.spec.js
@@ -6,7 +6,7 @@ jest.setTimeout(10000);
 describe('get files content function', () => {
   let readFileSpy;
   const mockReadFile = (url, encoding, cb) =>
-    setTimeout(() => cb(null, mockFileContent));
+    setTimeout(() => cb(null, mockFileContent), 0);
   const mockFilePaths = ['/test-file.js'];
   const mockFileContent = 'some content';
 
@@ -20,18 +20,24 @@ describe('get files content function', () => {
   });
 
   it('should get paths for modified files', async () => {
+    console.log('TEST 1');
+    const filePaths = await getFilesContent(mockFilePaths);
+    console.log('TEST 2');
     const expected = [
       { relativePath: '/test-file.js', content: 'some content' },
     ];
 
-    expect(await getFilesContent(mockFilePaths)).toEqual(expected);
     expect(readFileSpy).toHaveBeenCalled();
+    console.log('TEST 3');
+    expect(filePaths).toEqual(expected);
+    console.log('TEST 4');
   });
 
   it('should return empty array if no files exist', async () => {
+    const filePaths = await getFilesContent();
     const expected = [];
 
-    expect(await getFilesContent()).toEqual(expected);
     expect(readFileSpy).not.toHaveBeenCalled();
+    expect(filePaths).toEqual(expected);
   });
 });
