b2f9f62527737816462a01d563af296625c21b38
OmerB <omerb@wix.com>
2020-02-25 15:58:39 +0200

    Chore(sync-ts): integrate app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const getModifiedFiles = require('./get-modified-files');
+const verifyModifiedFiles = require('./verify-modified-files');
 
 const sourceBranch = process.argv[3];
 
-getModifiedFiles(sourceBranch)
-  .then(data => {
-    console.log('data', data);
+(async () => {
+  try {
+    const modifiedFiles = await getModifiedFiles(sourceBranch);
+    verifyModifiedFiles(modifiedFiles);
     process.exit(0);
-  })
-  .catch(err => {
+  } catch (err) {
     console.error(err); // eslint-disable-line no-console
     process.exit(1);
-  });
+  }
+})();
