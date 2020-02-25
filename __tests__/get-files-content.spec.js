7de8cd2ac22f03646d84a37aef325055e3b2a95c
OmerB <omerb@wix.com>
2020-02-25 16:31:32 +0200

    Fix(sync-ts): try to fix build

--- __tests__/get-files-content.spec.js
+++ __tests__/get-files-content.spec.js
@@ -1,6 +1,8 @@
 import fs from 'fs';
 import getFilesContent from '../src/get-modified-files/get-files-content';
 
+jest.setTimeout(10000);
+
 describe('get files content function', () => {
   let readFileSpy;
   const mockReadFile = (url, encoding, cb) =>
@@ -17,7 +19,7 @@ describe('get files content function', () => {
     readFileSpy.mockClear();
   });
 
-  it('should get paths for modified files', async done => {
+  it('should get paths for modified files', async () => {
     const filePaths = await getFilesContent(mockFilePaths);
     const expected = [
       { relativePath: '/test-file.js', content: 'some content' },
@@ -26,16 +28,14 @@ describe('get files content function', () => {
     expect(readFileSpy).toHaveBeenCalled();
     expect(filePaths.length).toBe(1);
     expect(filePaths).toEqual(expected);
-    done();
   });
 
-  it('should return empty array if no files exist', async done => {
+  it('should return empty array if no files exist', async () => {
     const filePaths = await getFilesContent();
     const expected = [];
 
     expect(readFileSpy).not.toHaveBeenCalled();
     expect(filePaths.length).toBe(0);
     expect(filePaths).toEqual(expected);
-    done();
   });
 });
