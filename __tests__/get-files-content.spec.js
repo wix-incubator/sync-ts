7dd0363558b4d9d67f646bb6e053e2d6a6261c90
OmerB <omerb@wix.com>
2020-02-25 16:59:51 +0200

    Chore(sync-ts): try to fix tests

--- __tests__/get-files-content.spec.js
+++ __tests__/get-files-content.spec.js
@@ -1,12 +1,9 @@
 import fs from 'fs';
 import getFilesContent from '../src/get-modified-files/get-files-content';
 
-jest.setTimeout(10000);
-
 describe('get files content function', () => {
   let readFileSpy;
-  const mockReadFile = (url, encoding, cb) =>
-    setTimeout(() => cb(null, mockFileContent), 0);
+  const mockReadFile = (url, encoding, cb) => cb(null, mockFileContent);
   const mockFilePaths = ['/test-file.js'];
   const mockFileContent = 'some content';
 
@@ -20,17 +17,13 @@ describe('get files content function', () => {
   });
 
   it('should get paths for modified files', async () => {
-    console.log('TEST 1');
     const filePaths = await getFilesContent(mockFilePaths);
-    console.log('TEST 2');
     const expected = [
       { relativePath: '/test-file.js', content: 'some content' },
     ];
 
     expect(readFileSpy).toHaveBeenCalled();
-    console.log('TEST 3');
     expect(filePaths).toEqual(expected);
-    console.log('TEST 4');
   });
 
   it('should return empty array if no files exist', async () => {
