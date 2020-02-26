d4ca438d61cba15d43e24df7c826b62d0a5f618c
OmerB <omerb@wix.com>
2020-02-26 16:11:24 +0200

    Chore(sync-ts): take args from env

--- src/index.js
+++ src/index.js
@@ -3,15 +3,19 @@
 const getModifiedFiles = require('./get-modified-files');
 const verifyModifiedFiles = require('./verify-modified-files');
 
-const sourceBranch = process.argv[3];
-
+const { sourceBranch, skip } = process.env;
 (async () => {
+  if (skip) {
+    console.log('sync-ts: skipped all checks.');
+    process.exit(0);
+  }
   try {
     const modifiedFiles = await getModifiedFiles(sourceBranch);
     verifyModifiedFiles(modifiedFiles);
+    console.log('sync-ts: all checks passed.');
     process.exit(0);
   } catch (err) {
-    console.error(err); // eslint-disable-line no-console
+    console.error(err);
     process.exit(1);
   }
 })();
