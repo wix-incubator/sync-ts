051fa00384425f51e80dd72e86198fcc8e80089f
OmerB <omerb@wix.com>
2020-02-26 12:49:20 +0200

    Chore(sync-ts): add tests for resolve files

---
+++ __tests__/resolve-files.spec.js
@@ -0,0 +1,38 @@
+import resolveFiles from '../src/get-modified-files/resolve-files';
+
+describe('resolve files function', () => {
+  it('should return an empty object for no files', async () => {
+    expect(resolveFiles()).toEqual({});
+    expect(resolveFiles([])).toEqual({});
+    expect(resolveFiles(null)).toEqual({});
+    expect(resolveFiles(null, null)).toEqual({});
+  });
+
+  it('should return a valid object for provided files', async () => {
+    const prFiles = [
+      { relativePath: '/file.js', content: 'some content' },
+      { relativePath: '/new-file.js', content: 'new content' },
+      { relativePath: '/deleted-file.js', content: '' },
+    ];
+    const sourceBranchFiles = [
+      { relativePath: '/file.js', content: 'some other content' },
+      { relativePath: '/new-file.js', content: '' },
+      { relativePath: '/deleted-file.js', content: 'deleted content' },
+    ];
+    const expected = {
+      '/file.js': {
+        contentFromPr: 'some content',
+        contentFromSourceBranch: 'some other content',
+      },
+      '/new-file.js': {
+        contentFromPr: 'new content',
+        contentFromSourceBranch: '',
+      },
+      '/deleted-file.js': {
+        contentFromPr: '',
+        contentFromSourceBranch: 'deleted content',
+      },
+    };
+    expect(resolveFiles(prFiles, sourceBranchFiles)).toEqual(expected);
+  });
+});
