5ec900e03e823702866ef9379ad03f216d1e08e1
Ziv Pizanti <zivp@wix.com>
2020-02-27 09:38:36 +0200

    some more messaging refactor

--- src/verify-modified-files/messages.js
+++ src/verify-modified-files/messages.js
@@ -1,11 +1,11 @@
 const messages = {
-  compAdded: () => 'An exported component was added to the file.',
-  compDeleted: () => 'An exported component was deleted from the file.',
+  compAdded: () => '\tAn exported component was added to the file.\n',
+  compDeleted: () => '\tAn exported component was deleted from the file.\n',
   verificationFailed: () =>
     '** SyncTS Failed for PR, please fix files listed above and re-run build. **',
   propsChanged: compName => `\tPropTypes were changed in ${compName} component\n`,
   fileChanged: (filePath, fileMessage) =>
-    `Error: Changes detected in ${filePath} - please update relevant index.d.ts file and push again.\nIssues to fix:\n${fileMessage}\n`,
+    `Error: Changes detected in ${filePath} - please update relevant index.d.ts file and push again.\nIssues to fix:\n${fileMessage}`,
 };
 
 module.exports = messages;
