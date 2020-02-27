9b38e760409675962aeae2265ac85be569bc3522
Ziv Pizanti <zivp@wix.com>
2020-02-27 10:16:40 +0200

    messages content change

--- src/verify-modified-files/messages.js
+++ src/verify-modified-files/messages.js
@@ -1,11 +1,11 @@
 const messages = {
-  compAdded: () => '\tAn exported component was added to the file.\n',
-  compDeleted: () => '\tAn exported component was deleted from the file.\n',
+  compAdded: () => '\t\tAn exported component was added to the file.\n',
+  compDeleted: () => '\t\tAn exported component was deleted from the file.\n',
   verificationFailed: () =>
     '** SyncTS Failed for PR, please fix files listed above and re-run build. **',
-  propsChanged: compName => `\tPropTypes were changed in ${compName} component\n`,
+  propsChanged: compName => `\t\tPropTypes were changed in ${compName} component.\n`,
   fileChanged: (filePath, fileMessage) =>
-    `Error: Changes detected in ${filePath} - please update relevant index.d.ts file and push again.\nIssues to fix:\n${fileMessage}`,
+    `SyncTS Error: Changes detected in ${filePath} - please update relevant index.d.ts file and push again.\nIssues to fix:\n${fileMessage}`,
 };
 
 module.exports = messages;
