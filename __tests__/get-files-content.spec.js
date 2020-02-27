d3dfddd9302e2cf1f038a5af289213f1946eaacf
OmerB <omerb@wix.com>
2020-02-27 12:26:45 +0200

    Chore(sync-ts): mock shell for test

--- __tests__/get-files-content.spec.js
+++ __tests__/get-files-content.spec.js
@@ -1,19 +1,25 @@
 const mockFs = require('mock-fs');
 
+import shell from 'shelljs';
 import getFilesContent from '../src/get-modified-files/get-files-content';
+import { mockExec } from '../drivers/mocks/shellJS';
 
 describe('get files content function', () => {
   const mockFilePath = '/test-file.js';
   const mockFileContent = 'some content';
+  let execSpy;
 
   beforeEach(() => {
     mockFs({
       mockFilePath: mockFileContent,
     });
+    execSpy = jest.spyOn(shell, 'exec');
+    execSpy.mockImplementation(mockExec({ mockData: './' }));
   });
 
   afterEach(() => {
     mockFs.restore();
+    execSpy.mockClear();
   });
 
   it('should get paths for modified files', async () => {
