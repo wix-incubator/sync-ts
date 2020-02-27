64342050cb26b719616bc3c9082fd9ba1ebb0da0
OmerB <omerb@wix.com>
2020-02-27 12:50:11 +0200

    Chore(sync-ts): check cwd

--- src/get-modified-files/get-files-content.js
+++ src/get-modified-files/get-files-content.js
@@ -3,6 +3,7 @@ const getGitRootPath = require('./get-git-root-path');
 
 const getFilesContent = async (filePaths = []) =>
   new Promise(async resolve => {
+    console.log('### cwd', process.cwd());
     const deferredFiles = filePaths.map(async relativePath => ({
       relativePath,
       content: await getFileContent(relativePath),
@@ -13,7 +14,6 @@ const getFilesContent = async (filePaths = []) =>
 async function getFileContent(filePath) {
   return new Promise(async resolve => {
     const gitRootPath = await getGitRootPath();
-    console.log('### cwd', process.cwd());
     console.log('### gitRootPath', gitRootPath);
     fs.readFile(`${gitRootPath}/${filePath}`, 'utf8', (err, data) => {
       if (!err) {
