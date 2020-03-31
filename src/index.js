04f7a908be2f2c27069ec207d41d702df452e6f1
moshek <moshek@wix.com>
2020-03-31 12:44:52 +0300

    use minimalist for better args (argv) parsing

--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
 #!/usr/bin/env node
-
+const minimist = require('minimist');
 const getModifiedFiles = require('./get-modified-files');
 const verifyModifiedFiles = require('./verify-modified-files');
 
-const {
-  sourceBranch,
-  excludePaths: excludePathsRaw,
-  skip = false,
-} = process.env;
 (async () => {
-  console.log(
-    `sync-ts params: sourceBranch=${sourceBranch} excludePaths=${excludePathsRaw} skip=${skip}`,
+  const args = minimist(process.argv.slice(2), {
+    string: ['sourceBranch', 'excludePaths'],
+    boolean: ['skip'],
+  });
+  const { sourceBranch, skip = false } = args;
+  const excludePaths = Array.isArray(args.excludePaths)
+    ? args.excludePaths
+    : (args.excludePaths
+    ? [args.excludePaths]
+    : []);
+  console.info(
+    `sync-ts params: sourceBranch=${sourceBranch} excludePaths=${excludePaths} skip=${skip}`,
   );
   if (skip) {
     console.info('sync-ts: all checks skipped.');
     process.exit(0);
   }
-  const excludePaths = excludePathsRaw
-    ? excludePathsRaw.split(';').filter(p => p.trim() !== '')
-    : [];
+
   try {
     const modifiedFiles = await getModifiedFiles(sourceBranch, excludePaths);
     verifyModifiedFiles(modifiedFiles);
